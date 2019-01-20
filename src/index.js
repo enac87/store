69569ce3433e56ddc5de144b0304b7b545443407
Emaga Alain <alaincesar@hotmail.fr>
2019-01-20 16:24:48 +0100

    Done ProductList

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import {BrowserRouter as Router} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import {ProductProvider} from './context';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, 
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>, 
   document.getElementById('root')
 );
 
